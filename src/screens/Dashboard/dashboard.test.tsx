caa97d69253252984667cd6068c1f8bb1cfb1b02
Carlos Alberto <carlosjunior.alberto4@gmail.com>
2024-10-30 08:41:48 -0300

    feat: learning to use fireEvent to press and change text, and wait for element to be remove with waitForElementToBeRemoved

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -1,6 +1,13 @@
 import { mockCityApiResponse } from "@__tests__/mocks/api/mockCityApiResponse";
 import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
-import { render, screen, waitFor } from "@__tests__/utils/customRender";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@__tests__/utils/customRender";
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 import { Dashboard } from "@screens/Dashboard";
 import { api } from "@services/api";
@@ -21,31 +28,50 @@ describe("Screen: Dashboard", () => {
     const { debug } = render(<Dashboard />);
 
     const cityName = await waitFor(() => screen.getByText(city.name));
-    debug();
     expect(cityName).toBeTruthy();
   });
 
   it("should be show another selected weather city", async () => {
-
     const city = {
-        id: "1",
-        name: "Rio do Sul",
-        latitude: 123,
-        longitude: 123,
-      };
-  
-      await saveStorageCity(city);
+      id: "1",
+      name: "Rio do Sul",
+      latitude: 123,
+      longitude: 123,
+    };
 
-      /**
-       * 1. Busca as informacoes do tempo/clima da cidade selecionada
-       * 2. Busca as informacoes da cidade
-       * 3. Busca as informacoes do tempo/clima da nova cidade selecionada
-       */
-      
-      jest.spyOn(api, "get")
-        .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
-        .mockResolvedValueOnce({ data: mockCityApiResponse })
-        .mockResolvedValueOnce({ data: mockWeatherAPIResponse });
-  }
-)
+    await saveStorageCity(city);
+
+    /**
+     * 1. Busca as informacoes do tempo/clima da cidade selecionada
+     * 2. Busca as informacoes da cidade
+     * 3. Busca as informacoes do tempo/clima da nova cidade selecionada
+     */
+
+    jest
+      .spyOn(api, "get")
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+      .mockResolvedValueOnce({ data: mockCityApiResponse })
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse });
+
+    const { debug } = render(<Dashboard />);
+
+    await waitForElementToBeRemoved(() => screen.getByTestId("loading"));
+
+    const cityName = "Fortaleza";
+
+    await waitFor(() =>
+      act(() => {
+        const search = screen.getByTestId("search-input");
+        fireEvent.changeText(search, cityName);
+      })
+    );
+
+    await waitFor(() =>
+      act(() => {
+        fireEvent.press(screen.getByText(cityName, { exact: false }));
+      })
+    );
+
+    expect(screen.getByText(cityName, { exact: false })).toBeTruthy();
+  });
 });
